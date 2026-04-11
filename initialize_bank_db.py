b1523c0ed0c7444013b06ffd9ee02e064bc0eedf
Enrique Uribe <ukikke@gmail.com>
2026-04-11 02:23:54 +0000

    completed python menu selection with complete functions

--- initialize_bank_db.py
+++ initialize_bank_db.py
@@ -5,11 +5,14 @@ DB_NAME = 'example.db'
 
 def initialize_bank_database():
     connection = sqlite3.connect(DB_NAME)
-    print("Connected to the database.")
+    #print("Connected to the database.")
     cursor = connection.cursor()
-    print("Cursor created.")
+    #print("Cursor created.")
+
+    cursor.execute('''
+        DROP TABLE IF EXISTS bank_accounts;''')
     # Create a sample table
-    print("Creating table if it does not exist...")
+    #print("Creating table if it does not exist...")
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS bank_accounts
             (id integer primary key, 
@@ -18,21 +21,21 @@ def initialize_bank_database():
             account_type text)
     ''')
 
-    print("Table created.")
+    #print("Table created.")
 
     # Insert sample data
-    print("Inserting sample data...")
+    #print("Inserting sample data...")
     cursor.execute('''
         INSERT INTO bank_accounts (id, name, dollar_amount, account_type) VALUES
         (1, 'Alice', 1000.0, 'Checking'),
         (2, 'Bob', 2000.0, 'Savings'),
         (3, 'Charlie', 1500.0, 'Checking')
     ''')
-    print("Sample data inserted.")
+    #print("Sample data inserted.")
     # Commit the changes and close the connection
-    print("Committing changes and closing the connection...")
+    #print("Committing changes and closing the connection...")
     connection.commit()
     connection.close()
 
 
-initialize_bank_database()
+#initialize_bank_database()
--- menu_bank_db.py
+++ menu_bank_db.py
@@ -1,12 +1,13 @@
 import sqlite3
-user_name = "Family_accounts_love"
+import initialize_bank_db
+user_name = "Family"
 password = "password123"
 database_name = "bank_accounts"
 
-id = 0
-name = ""
-account_amount = 0.0
-account_type = ""
+id1 = 0
+name1 = ""
+account_amount1 = 0.0
+account_type1 = ""
 
 def security_check():
     input_username = input("Enter username: ")
@@ -28,14 +29,60 @@ def bank_menu():
     print("5. Check account balance")
     print("6. Exit")
 
+def selection():
+    number = int(input('Enter the number of your choice[1-5]:  '))
+    if number == 1:
+        view_account()
+    elif number == 2:
+        create_account()
+    elif number == 3:
+        deposit_into_account()
+    elif number == 4:
+        withdraw_into_account()
+    elif number == 5:
+        check_account_balance()
+    elif number == 6:
+        print(f"Have a great day!")
+        return 1
+    return 0
 
-
-def main():
+def view_account():
     connection = sqlite3.connect('example.db')
     cursor = connection.cursor()
 
     # Get all rows from the Bank table
     print("Fetching all rows from the bank_accounts table...")
+    results = cursor.execute(f'''
+        SELECT id, name, account_type FROM {database_name}
+    ''')
+
+    print("Results:")
+    for row in results:
+        print(row)
+
+    connection.close()
+
+def create_account():
+    connection = sqlite3.connect('example.db')
+    cursor = connection.cursor()
+    id1 = 1
+
+    results = cursor.execute(f'''
+        SELECT * FROM {database_name}
+    ''')
+
+    for i in results:
+        id1 += 1
+
+    name1 = input("Name of bank account user: ")
+    initial_deposit1 = float(input("How much would you like to deposit: "))
+    account_type1 = input("What will the type(Savings or Checking): ")
+
+    results = cursor.execute(f'''
+       INSERT INTO bank_accounts (id, name, dollar_amount, account_type) VALUES
+        ({id1}, '{name1}', {initial_deposit1}, '{account_type1}');
+    ''')
+
     results = cursor.execute(f'''
         SELECT * FROM {database_name}
     ''')
@@ -44,18 +91,95 @@ def main():
     for row in results:
         print(row)
 
-    # Get all bank accounts with a balance greater than 1000
-    print("Fetching bank accounts with balance greater than 1000...")
-    results = cursor.execute(f'''
-        SELECT * FROM {database_name} WHERE dollar_amount > 1000
+    connection.commit()
+    connection.close()
+
+def id_checker(id):
+    connection = sqlite3.connect('example.db')
+    cursor = connection.cursor()
+
+    list_of_ID = cursor.execute(f'''
+        SELECT id FROM {database_name}
     ''')
+
+    for ids in list_of_ID:
+        if f"({id},)" == str(ids):
+            return True    
+    connection.close()
+
+def current_amount(id):
+    connection = sqlite3.connect('example.db')
+    cursor = connection.cursor()
+
+    account_amount1 = cursor.execute(f'''
+        SELECT dollar_amount FROM {database_name} WHERE id = {id}
+    ''')
+    for i in account_amount1:
+        return i[0]
+    connection.close()
+
+def deposit_into_account():
+    view_account()
+    id1 = int(input("Select the id of the account you want to deposit money: "))
+    connection = sqlite3.connect('example.db')
+    cursor = connection.cursor()
+    if id_checker(id1):
+        amount_depositing = float(input("How much would you like to deposit: "))
+        cursor.execute(f'''
+        UPDATE {database_name}
+        SET dollar_amount = {current_amount(id1) + amount_depositing}
+        WHERE id = {id1};
+        ''')
+    else:
+        print("Sorry try again")
+    connection.commit()
+    connection.close()
+
+def withdraw_into_account():
+    view_account()
+    id1 = int(input("Select the id of the account you want to withdraw money: "))
+    connection = sqlite3.connect('example.db')
+    cursor = connection.cursor()
+    if id_checker(id1):
+        amount_depositing = float(input("How much would you like to withdraw: "))
+        cursor.execute(f'''
+        UPDATE {database_name}
+        SET dollar_amount = {current_amount(id1) - amount_depositing}
+        WHERE id = {id1};
+        ''')
+    else:
+        print("Sorry try again")
+    connection.commit()
+    connection.close()
+
+def reset_database():
+    initialize_bank_db.initialize_bank_database()
+
+def check_account_balance():
+    view_account()
+    id1 = int(input("Select the id of the account you want deposit to see new balance: "))
+    connection = sqlite3.connect('example.db')
+    cursor = connection.cursor()
+
+    # Get all rows from the Bank table
+    print("Fetching information from the bank_accounts table...")
+    results = cursor.execute(f'''
+        SELECT name, dollar_amount FROM {database_name} WHERE id = {id1}
+    ''')
+
     print("Results:")
     for row in results:
         print(row)
 
     connection.close()
 
+reset_database()
+
 safe = True
 while safe:
     safe = security_check()
-main()
+
+loop = 0
+while loop == 0:
+    bank_menu()
+    loop = selection()
